8026c6b5a894e305bb70f5310214f347ee6c2804
Shafkat Islam <shafkatislam.12@gmail.com>
2025-12-02 01:34:24 +0600

    vedio updated

--- script/index.js
+++ script/index.js
@@ -2,54 +2,33 @@ fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
   .then((res) => res.json())
   .then((data) => displayCategories(data.categories));
 
-//   "category_id": "1001",
-// "category": "Music"
-
 function displayCategories(categories) {
   const buttonContainer = document.getElementById("buttons");
 
   for (let item of categories) {
-    console.log(item);
+    // console.log(item);
 
     // create a button
     const button = document.createElement("button");
     button.classList = "btn";
     button.innerText = item.category;
 
+    console.log(item.category_id);
+
     // add button to category container
     buttonContainer.appendChild(button);
   }
 }
 
-// authors
-// :
-// [{…}]
-// category_id
-// :
-// "1001"
-// description
-// :
-// "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
-// others
-// :
-// {views: '100K', posted_date: '16278'}
-// thumbnail
-// :
-// "https://i.ibb.co/L1b6xSq/shape.jpg"
-// title
-// :
-// "Shape of You"
-// video_id
-// :
-// "aaaa"
-
-//fetching vedio api
+// function to load all ther vedios
 
 function loadVideos() {
   fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
     .then((res) => res.json())
     .then((data) => showVideos(data.videos));
 }
+
+// function to show all the videos
 
 const showVideos = (vids) => {
   console.log(vids);
@@ -61,20 +40,37 @@ const showVideos = (vids) => {
     // console.log(vid);
     const newCard = document.createElement("div");
     newCard.innerHTML = `
-    <div class="card bg-base-100 w-96 shadow-sm">
-  <figure>
-    <img
-      src= ${vid.thumbnail}
-      alt="Shoes" />
-  </figure>
-  <div class="card-body">
-    <h2 class="card-title">${vid.title}</h2>
-    <p>${vid.description}</p>
-    <div class="card-actions justify-end">
-      <button class="btn btn-primary">Buy Now</button>
-    </div>
-  </div>
-</div>
+     <div class="card bg-base-100 w-96 shadow-sm flex flex-col gap-5 h-auto">
+        <figure>
+          <img class = "w-full h-[200px] object-cover" src=${vid.thumbnail} alt="Shoes" />
+        </figure>
+        <!-- card body -->
+        <div class="avatar h-[180px] flex gap-10">
+          <div
+            id="profile-img"
+            class="ring-primary h-12 w-12 rounded-full ml-5 mt-5 mr-0"
+          >
+            <img
+              class=""
+              src= ${vid.authors[0].profile_picture}
+            />
+          </div>
+          <div class="profile-info w-full">
+            <h2 class="font-bold text-xl pb-2">
+              ${vid.title}
+            </h2>
+            <p class="flex text-sm text-gray-400">
+              ${vid.authors[0].profile_name}
+              <img
+                class="h-5 w-5 ml-2"
+                src="https://img.icons8.com/?size=96&id=U8PNLNMhOOtq&format=png"
+                alt=""
+              />
+            </p>
+            <p class="text-gray-400 text-sm">${vid.others.views} views</p>
+          </div>
+        </div>
+      </div>
 
   `;
     vedioContainer.appendChild(newCard);
@@ -83,4 +79,4 @@ const showVideos = (vids) => {
   //vedioContainer.appendChild(newCard);
 };
 
-loadVideos();
+//loadVideos();
