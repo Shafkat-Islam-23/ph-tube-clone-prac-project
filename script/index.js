c28eea1c623a5889cd1307a0023f393800a6d86e
Shafkat Islam <shafkatislam.12@gmail.com>
2025-12-05 00:01:44 +0600

    search bar updated

--- script/index.js
+++ script/index.js
@@ -11,14 +11,14 @@ function displayCategories(categories) {
     button.innerText = item.category;
     button.onclick = () => {
       //document.getElementsByClassName("btn").;
-      button.classList.remove("btn");
+      button.classList.remove("btn-nav");
       // removing style from other buttons
 
       for (let item of categories) {
-        button.classList.remove("btn");
+        button.classList.remove("btn-nav");
       }
 
-      button.classList.add("btn");
+      button.classList.add("btn-nav");
       loadCatagoryVideos(item.category_id);
     };
 
@@ -28,8 +28,10 @@ function displayCategories(categories) {
 
 // function to load all ther vedios
 
-function loadVideos() {
-  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+function loadVideos(search = "") {
+  fetch(
+    `https://openapi.programming-hero.com/api/phero-tube/videos?title=${search}`
+  )
     .then((res) => res.json())
     .then((data) => showVideos(data.videos));
 }
@@ -70,16 +72,18 @@ const showVideos = (vids) => {
   const newCard = document.createElement("div");
 
   vids.forEach((vid) => {
-    // console.log(vid);
+    //console.log(vid);
 
     const newCard = document.createElement("div");
     newCard.innerHTML = `
      <div class="card bg-base-100 w-96 shadow-sm flex flex-col gap-5 h-auto">
         <figure>
-          <img class = "w-full h-[200px] object-cover" src=${vid.thumbnail} alt="Shoes" />
+          <img class = "w-full h-[200px] object-cover" src=${
+            vid.thumbnail
+          } alt="Shoes" />
         </figure>
         <!-- card body -->
-        <div class="avatar h-[180px] flex gap-10">
+        <div class="avatar h-[120px] flex gap-10">
           <div
             id="profile-img"
             class="ring-primary h-12 w-12 rounded-full ml-5 mt-5 mr-0"
@@ -95,15 +99,30 @@ const showVideos = (vids) => {
             </h2>
             <p class="flex text-sm text-gray-400">
               ${vid.authors[0].profile_name}
-              <img
+            ${
+              vid.authors[0].verified == true
+                ? ` <img
                 class="h-5 w-5 ml-2"
                 src="https://img.icons8.com/?size=96&id=U8PNLNMhOOtq&format=png"
                 alt=""
-              />
+              />`
+                : ` <img
+                class="h-5 w-5 ml-2"
+                src="https://img.icons8.com/?size=100&id=owIPfgUDQ92m&format=png&color=000000"
+                alt=""
+              />`
+            }
+              
             </p>
             <p class="text-gray-400 text-sm">${vid.others.views} views</p>
           </div>
         </div>
+        
+       <div  class = 'flex justify-center items-center pb-5'> <button onclick =loadVideoDetails('${
+         vid.video_id
+       }') class="btn btn-wide  ">Wide</button></div>
+       
+        
       </div>
 
   `;
@@ -112,3 +131,31 @@ const showVideos = (vids) => {
 };
 
 //loadVideos();
+
+const loadVideoDetails = (id) => {
+  // console.log("aaa");
+  console.log(id);
+
+  const url = `https://openapi.programming-hero.com/api/phero-tube/video/${id}`;
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => showVedioDetails(data.video.description));
+};
+
+const showVedioDetails = (details) => {
+  console.log(details);
+
+  document.getElementById("vedio_details").showModal();
+
+  const detailsContainer = document.getElementById("details_container");
+  detailsContainer.innerHTML = `
+  <p class = 'text-xl font-bold'>${details}</p>
+  `;
+};
+
+document.getElementById("input_search").addEventListener("keyup", (e) => {
+  const searchInput = e.target.value;
+  loadVideos(searchInput);
+  console.log(searchInput);
+});
